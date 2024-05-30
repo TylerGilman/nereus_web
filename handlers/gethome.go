b6d79a071592c8dce88b401a638a31bf2e742637
Tyler Gilman <tylersgilman@gmail.com>
2024-05-30 15:24:41 -0400

    workingy

--- handlers/gethome.go
+++ handlers/gethome.go
@@ -7,6 +7,7 @@ import (
 
 	"nereus_web/views/about"
 	"nereus_web/views/blog"
+	"nereus_web/views/components"
 	"nereus_web/views/contact"
 	"nereus_web/views/home"
 )
@@ -24,12 +25,15 @@ func HandleHome(w http.ResponseWriter, r *http.Request) error {
 	header := r.Header.Get("View")
 	if header == "content" {
 		if tab == "About" {
+			Render(w, r, components.Navigation(false, 0))
 			return Render(w, r, about.About())
 		}
 		if tab == "Contact" {
+			Render(w, r, components.Navigation(false, 1))
 			return Render(w, r, contact.Contact())
 		}
 		if tab == "Blog" {
+			Render(w, r, components.Navigation(false, 2))
 			return Render(w, r, blog.Blog())
 		}
 	}
